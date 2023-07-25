f9f09ede6cdad2c12ef80302582ffe7c4376ce79
0521kimyi <0521kimyi@gmail.com>
2023-07-25 16:34:01 +0900

    1부터 사용자가 입력한 n 사이의 모든 소수 찾도록 수정

--- Chapter08_함수/Chapter08_함수/prime.c
+++ Chapter08_함수/Chapter08_함수/prime.c
@@ -11,9 +11,12 @@ int get_integer() {
 }
 
 int is_prime(int n) {
-	int i;
+	//int i;
 
-	for (i = 2; i < n; i++) {
+	if (n <= 1)
+		return 0;
+
+	for (int i = 2; i < n; i++) {
 		if (n % i == 0)
 			return 0;
 	}
@@ -24,10 +27,17 @@ int main() {
 	int n, result;
 	n = get_integer();
 
-	if (is_prime(n) == 1)
+	/*if (is_prime(n) == 1)
 		printf("%d은 소수입니다.\n", n);
 	else
 		printf("%d은 소수가 아닙니다.\n", n);
-	return 0;
+	return 0;*/
+
+	printf("1부터 %d 사이의 소수: ", n);
+	for (int i = 2; i <= n; i++) {
+		if (is_prime(i) == 1)
+			printf("%d ", i);
+	}
+	printf("\n");
 }
 
