c7aafd25ec7a8d0cc107c74fd5771b7916a9fc55
0521kimyi <0521kimyi@gmail.com>
2023-07-25 15:17:56 +0900

    생일 축하 함수

---
+++ Chapter08_함수/Chapter08_함수/func_exam2.c
@@ -0,0 +1,15 @@
+/* 생일 축하 함수 */
+
+#include <stdio.h>
+
+void happyBirthday() {
+	printf("생일축하합니다! \n");
+	printf("생일축하합니다! \n");
+	printf("사랑하는 친구의 ");
+	printf("생일축하합니다! \n");
+}
+
+int main(void) {
+	happyBirthday();
+	return 0;
+}
