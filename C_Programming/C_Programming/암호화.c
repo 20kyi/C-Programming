63588ad11848e25da05560b8da7cacba484721f9
0521kimyi <0521kimyi@gmail.com>
2023-07-25 14:06:22 +0900

    암호화 코드 작성

---
+++ C_Programming/C_Programming/암호화.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+#define SECRETKEY "!@#$%^&*()_+=-"
+
+int main() {
+	int i;
+	char string[] = "1234567890";
+
+	puts(string);
+
+	for (i = 0; i < strlen(string); i++) {
+		string[i] ^= SECRETKEY[i];
+	}
+
+	puts(string);
+
+	for (i = 0; i < strlen(string); i++) {
+		string[i] ^= SECRETKEY[i];
+	}
+
+	puts(string);
+
+	return 0;
+}
