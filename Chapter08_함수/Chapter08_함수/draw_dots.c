a8761d455fa17e96762b5376aec6dfa062c8be64
0521kimyi <0521kimyi@gmail.com>
2023-08-03 20:59:27 +0900

    불규칙하게 점 그리기

---
+++ Chapter08_함수/Chapter08_함수/draw_dots.c
@@ -0,0 +1,28 @@
+/* 불규칙하게 점 그리기 */
+
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+	int i, x, y, red, green, blue;
+
+	HDC hdc;
+	hdc = GetWindowDC(GetForegroundWindow());
+
+	srand((unsigned)time(NULL));
+	for (i = 0; i < 10000; i++) {
+		x = rand() % 300;	// x는 0에서 299 사이의 랜덤한 값
+		y = rand() % 300;	// y는 0에서 299 사이의 랜덤한 값
+
+		red = rand() % 256;	// red는 0에서 255 사이의 랜덤한 값
+		green = rand() % 256; // green은 0에서 255 사이의 랜덤한 값
+		blue = rand() % 256; // blue는 0에서 255 사이의 랜덤한 값
+
+		SetPixel(hdc, x, y, RGB(red, green, blue));
+	}
+
+	_getch();
+	return 0;
+}
